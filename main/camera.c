0e3ef08de1964059ecd5d33fc2b794fe412d35bd
mauriciobarroso <mauriciobarroso1990@gmail.com>
2022-10-10 22:16:32 -0400

    cleaned code

--- main/camera.c
+++ main/camera.c
@@ -1,22 +1,52 @@
-/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+/**
+  ******************************************************************************
+  * @file           : camera.c
+  * @author         : Mauricio Barroso Benavides
+  * @date           : Oct 2, 2022
+  * @brief          : todo: write brief
+  ******************************************************************************
+  * @attention
+  *
+  * MIT License
+  *
+  * Copyright (c) 2022 Mauricio Barroso Benavides
+  *
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+  * of this software and associated documentation files (the "Software"), to
+  * deal in the Software without restriction, including without limitation the
+  * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+  * sell copies of the Software, and to permit persons to whom the Software is
+  * furnished to do so, subject to the following conditions:
+  *
+  * The above copyright notice and this permission notice shall be included in
+  * all copies or substantial portions of the Software.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+  * IN THE SOFTWARE.
+  *
+  ******************************************************************************
+  */
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-
+/* Includes ------------------------------------------------------------------*/
 #include "camera.h"
 
+/* Private macro -------------------------------------------------------------*/
+
+/* External variables --------------------------------------------------------*/
+
+/* Private typedef -----------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
 static const char *TAG = "camera";
 
+/* Private function prototypes -----------------------------------------------*/
+
+/* Exported functions --------------------------------------------------------*/
 int camera_init(void) {
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
@@ -55,3 +85,7 @@ int camera_init(void) {
 
   return 0;
 }
+
+/* Private functions ---------------------------------------------------------*/
+
+/***************************** END OF FILE ************************************/
--- main/camera.h
+++ main/camera.h
@@ -1,26 +1,53 @@
-/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+/**
+  ******************************************************************************
+  * @file           : camera.h
+  * @author         : Mauricio Barroso Benavides
+  * @date           : Oct 2, 2022
+  * @brief          : todo: write brief
+  ******************************************************************************
+  * @attention
+  *
+  * MIT License
+  *
+  * Copyright (c) 2022 Mauricio Barroso Benavides
+  *
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+  * of this software and associated documentation files (the "Software"), to
+  * deal in the Software without restriction, including without limitation the
+  * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+  * sell copies of the Software, and to permit persons to whom the Software is
+  * furnished to do so, subject to the following conditions:
+  *
+  * The above copyright notice and this permission notice shall be included in
+  * all copies or substantial portions of the Software.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+  * IN THE SOFTWARE.
+  *
+  ******************************************************************************
+  */
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+/* Define to prevent recursive inclusion -------------------------------------*/
 
 #ifndef CAMERA_H_
 #define CAMERA_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
 #include "sensor.h"
 #include "esp_camera.h"
 #include "esp_log.h"
 #include "esp_system.h"
 
+/* Exported macro ------------------------------------------------------------*/
 /**
  * PIXFORMAT_RGB565,    // 2BPP/RGB565
  * PIXFORMAT_YUV422,    // 2BPP/YUV422
@@ -69,14 +96,17 @@ limitations under the License.
 
 #define XCLK_FREQ_HZ 15000000
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+/* Exported typedef ----------------------------------------------------------*/
 
+/* Exported variables --------------------------------------------------------*/
+
+/* Exported functions prototypes ---------------------------------------------*/
 int camera_init(void);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif CAMERA_H_
+#endif /* CAMERA_H_ */
+
+/***************************** END OF FILE ************************************/
