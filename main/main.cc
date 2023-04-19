41fb79c06056df9b13134e9adb3c73873743d2e9
Mauricio Barroso Benavides <mauriciobarroso1990@gmail.com>
2023-04-19 10:42:04 -0400

    updated app code

--- main/main.cc
+++ main/main.cc
@@ -33,6 +33,9 @@
   */
 
 /* Includes ------------------------------------------------------------------*/
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "esp_log.h"
 #include "esp_system.h"
 #include "esp_heap_caps.h"
@@ -43,9 +46,14 @@
 
 #include "sensor.h"
 #include "esp_camera.h"
-#include "esp_log.h"
-#include "esp_system.h"
 #include "driver/ledc.h"
+
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "esp_spiffs.h"
+#include "esp_smartconfig.h"
+#include "file_server.c"
 
 #include "utils.h"
 #include "models/pnet_1.c"
@@ -75,12 +83,29 @@ tflite::MicroInterpreter * rnet_interpreter = nullptr;
 tflite::MicroInterpreter * onet_interpreter = nullptr;
 
 /* Private functions declaration ---------------------------------------------*/
-static void tflm_init(void);
+static esp_err_t nvs_init(void);
+static esp_err_t wifi_init(void);
+static esp_err_t spiffs_init(void);
 static esp_err_t camera_init(void);
+
+/* Event handlers */
+static void wifi_event_handler(void * arg, esp_event_base_t event_base,
+		int32_t event_id, void * event_data);
+static void ip_event_handler(void * arg, esp_event_base_t event_base,
+		int32_t event_id, void * event_data);
+static void sc_event_handler(void * arg, esp_event_base_t event_base,
+		int32_t event_id, void * event_data);
+
+static void tflm_init(void);
 static void inference_task(void * arg);
 
 /* Private user code ---------------------------------------------------------*/
 extern "C" void app_main() {
+	nvs_init();
+	wifi_init();
+	spiffs_init();
+	file_server_init();
+
 	/* Initialize TFLM and allocate tensors for the models */
 	tflm_init();
 
@@ -97,6 +122,194 @@ extern "C" void app_main() {
 }
 
 /* Private functions definition ----------------------------------------------*/
+static esp_err_t nvs_init(void) {
+	esp_err_t ret;
+
+	ESP_LOGI(TAG, "Initializing NVS...");
+
+    ret = nvs_flash_init();
+
+    if(ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    	ret = nvs_flash_erase();
+
+    	if(ret == ESP_OK) {
+    		ret = nvs_flash_init();
+
+    		if(ret != ESP_OK) {
+    			ESP_LOGE(TAG, "Error initializing NVS");
+
+    			return ret;
+    		}
+    	}
+    	else {
+    		ESP_LOGE(TAG, "Error erasing NVS");
+
+    		return ret;
+    	}
+
+    }
+
+	return ret;
+}
+
+static esp_err_t wifi_init(void) {
+	esp_err_t ret;
+
+	ESP_LOGI(TAG, "Initializing Wi-Fi...");
+
+	/* Initialize stack TCP/IP */
+	ret = esp_netif_init();
+
+	if (ret != ESP_OK) {
+		return ret;
+	}
+
+	/* Create event loop */
+	ret = esp_event_loop_create_default();
+
+	if (ret != ESP_OK) {
+		return ret;
+	}
+
+	/* Create netif instances */
+	esp_netif_create_default_wifi_sta();
+
+	/* Initialize Wi-Fi driver*/
+	wifi_init_config_t wifi_config = WIFI_INIT_CONFIG_DEFAULT();
+	esp_wifi_init(&wifi_config);
+
+	/* Declare event handler instances for Wi-Fi and IP */
+	esp_event_handler_instance_t instance_any_wifi;
+	esp_event_handler_instance_t instance_got_ip;
+	esp_event_handler_instance_t instance_got_sc;
+
+	/* Register Wi-Fi, IP and SmartConfig event handlers */
+	ret = esp_event_handler_instance_register(WIFI_EVENT,
+			ESP_EVENT_ANY_ID,
+			&wifi_event_handler,
+			NULL,
+			&instance_any_wifi);
+
+	if (ret != ESP_OK) {
+		return ret;
+	}
+
+	ret = esp_event_handler_instance_register(IP_EVENT,
+			IP_EVENT_STA_GOT_IP,
+			&ip_event_handler,
+			NULL,
+			&instance_got_ip);
+
+	if (ret != ESP_OK) {
+		return ret;
+	}
+
+	ret = esp_event_handler_instance_register(SC_EVENT,
+			ESP_EVENT_ANY_ID,
+			&sc_event_handler,
+			NULL,
+			&instance_got_sc);
+
+	if (ret != ESP_OK) {
+		return ret;
+	}
+
+	/* Set Wi-Fi mode */
+	ret = esp_wifi_set_mode(WIFI_MODE_STA);
+
+	if (ret != ESP_OK) {
+		return ret;
+	}
+
+
+	/* Start Wi-Fi */
+	ret = esp_wifi_start();
+
+	if (ret != ESP_OK) {
+		return ret;
+	}
+
+	/* Check if are Wi-Fi credentials provisioned */
+	wifi_config_t wifi_conf;
+	ret = esp_wifi_get_config(WIFI_IF_STA, &wifi_conf);
+
+	if (ret == ESP_OK) {
+		if (strlen((char *)wifi_conf.sta.ssid) > 0) {
+			ESP_LOGI(TAG, "Found Wi-Fi credentials in NVS");
+			ESP_LOGI(TAG, "SSID: %s", wifi_conf.sta.ssid);
+			ESP_LOGI(TAG, "Password: %s", wifi_conf.sta.password);
+
+			ESP_LOGI(TAG, "Connecting...");
+			esp_wifi_set_config(WIFI_IF_STA, &wifi_conf);
+			esp_wifi_connect();
+		}
+
+		else {
+			ESP_LOGI(TAG, "Not found Wi-Fi credentials in NVS. Starting SmartConfig...");
+
+			ret = esp_smartconfig_set_type(SC_TYPE_ESPTOUCH);
+
+			if (ret != ESP_OK) {
+				return ret;
+			}
+
+			smartconfig_start_config_t sc_config = SMARTCONFIG_START_CONFIG_DEFAULT();
+			ret = esp_smartconfig_start(&sc_config);
+
+			if (ret != ESP_OK) {
+				return ret;
+			}
+		}
+	}
+
+	return ret;
+}
+
+static esp_err_t spiffs_init(void) {
+	esp_err_t ret;
+
+	esp_spiffs_format(NULL);
+    ESP_LOGI(TAG, "Initializing SPIFFS...");
+
+    esp_vfs_spiffs_conf_t conf = {
+      .base_path = "/spiffs",
+      .partition_label = NULL,
+      .max_files = 5,
+      .format_if_mount_failed = true
+    };
+
+    ret = esp_vfs_spiffs_register(&conf);
+
+    if(ret != ESP_OK) {
+        if(ret == ESP_FAIL) {
+            ESP_LOGE(TAG, "Failed to mount or format filesystem");
+        }
+        else if(ret == ESP_ERR_NOT_FOUND) {
+            ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+        }
+        else {
+            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+        }
+
+        return ret ;
+    }
+
+    size_t total = 0, used = 0;
+
+    ret = esp_spiffs_info(conf.partition_label, &total, &used);
+
+    if(ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
+
+        return ret;
+    }
+    else {
+        ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
+    }
+
+	return ret;
+}
+
 static void tflm_init(void) {
 //	tflite::ErrorReporter* error_reporter = nullptr;
 //  tflite::MicroErrorReporter micro_error_reporter;
@@ -272,7 +485,7 @@ static void inference_task(void * arg) {
 		/* Convert to RGB888 and return frame buffer*/
 	  uint8_t * rgb888_image = (uint8_t *)malloc((IMG_W * IMG_H * IMG_CH) * sizeof(uint8_t));
 	  fmt2rgb888(fb->buf, fb->len, PIXFORMAT_RGB565, rgb888_image);
-	  esp_camera_fb_return(fb);
+//	  esp_camera_fb_return(fb);
 
 	  /* Run P-Net for all scales */
 	  candidate_windows_t pnet_candidate_windows;
@@ -341,16 +554,122 @@ static void inference_task(void * arg) {
 		printf("Time for O-Net = %lld\n", onet_time / 1000);
 		printf("Ouput bboxes:%d\n", onet_bboxes->len);
 
+
 		/* Print final results */
-	  draw_rectangle_rgb888(rgb888_image, onet_bboxes, IMG_W);
+		if (onet_bboxes->len > 0) {
+			draw_rectangle_rgb888(rgb888_image, onet_bboxes, IMG_W);
+			print_rgb888(rgb888_image, IMG_W, IMG_H);
+
+			/* Open file */
+			FILE * f = fopen("/spiffs/faces.jpg", "wb");
+			size_t cnv_buf_len;
+			uint8_t * cnv_buf = NULL;
+
+			fmt2jpg(rgb888_image, 96 * 96, 96, 96, PIXFORMAT_RGB888, 80, &cnv_buf, &cnv_buf_len);
+
+			fwrite(cnv_buf, cnv_buf_len, 1, f);
+			fclose(f);
+			free(cnv_buf);
+		}
+
 	  free(onet_bboxes->bbox);
 	  free(onet_bboxes);
-	  print_rgb888(rgb888_image, IMG_W, IMG_H);
 
 	  printf("MTCNN time = %lld\n\n", (pnet_time + rnet_time + onet_time) / 1000);
 
 	  free(rgb888_image);
+	  esp_camera_fb_return(fb);
 	  vTaskDelay(pdMS_TO_TICKS(20)); /* To avoid watchdog */
+	}
+}
+
+/* Event handlers */
+static void wifi_event_handler(void * arg, esp_event_base_t event_base,
+							   int32_t event_id, void * event_data) {
+	switch (event_id) {
+		case WIFI_EVENT_STA_START: {
+			ESP_LOGI(TAG, "WIFI_EVENT_STA_START");
+
+			break;
+		}
+
+		case WIFI_EVENT_STA_CONNECTED: {
+			ESP_LOGI(TAG, "WIFI_EVENT_STA_CONNECTED");
+
+			break;
+		}
+
+		case WIFI_EVENT_STA_DISCONNECTED: {
+			ESP_LOGI(TAG, "WIFI_EVENT_STA_DISCONNECTED");
+
+	        break;
+		}
+
+		default:
+			ESP_LOGI(TAG, "Other Wi-Fi event");
+			break;
+	}
+}
+
+static void ip_event_handler(void * arg, esp_event_base_t event_base,
+		int32_t event_id, void * event_data) {
+	switch (event_id) {
+		case IP_EVENT_STA_GOT_IP: {
+			ESP_LOGI(TAG, "IP_EVENT_STA_GOT_IP");
+
+			break;
+		}
+
+		default: {
+			ESP_LOGI(TAG, "Other IP event");
+
+			break;
+		}
+	}
+}
+
+static void sc_event_handler(void * arg, esp_event_base_t event_base,
+		int32_t event_id, void * event_data) {
+	switch (event_id) {
+		case SC_EVENT_GOT_SSID_PSWD: {
+			ESP_LOGI(TAG, "SC_EVENT_GOT_SSID_PSWD");
+
+			smartconfig_event_got_ssid_pswd_t * evt = (smartconfig_event_got_ssid_pswd_t *)event_data;
+			wifi_config_t wifi_config;
+
+			/* Copy obtained Wi-Fi credentials in Wi-Fi configurarion variable */
+			bzero(&wifi_config, sizeof(wifi_config_t));
+			memcpy(wifi_config.sta.ssid, evt->ssid, sizeof(wifi_config.sta.ssid));
+			memcpy(wifi_config.sta.password, evt->password, sizeof(wifi_config.sta.password));
+			wifi_config.sta.bssid_set = evt->bssid_set;
+
+			if (wifi_config.sta.bssid_set == true) {
+					memcpy(wifi_config.sta.bssid, evt->bssid, sizeof(wifi_config.sta.bssid));
+			}
+
+			/* Print Wi-Fi credentials */
+			ESP_LOGI(TAG, "SSID: %s", wifi_config.sta.ssid);
+			ESP_LOGI(TAG, "Password: %s", wifi_config.sta.password);
+
+			/* Configure Wi-Fi and try to connect */
+			ESP_ERROR_CHECK(esp_wifi_disconnect());
+			ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+			esp_wifi_connect();
+
+			break;
+		}
+		case SC_EVENT_SEND_ACK_DONE: {
+			ESP_LOGI(TAG, "SC_EVENT_SEND_ACK_DONE");
+
+			esp_smartconfig_stop();
+
+			break;
+		}
+
+		default:
+			ESP_LOGI(TAG, "Other SmartConfig event");
+
+			break;
 	}
 }
 
